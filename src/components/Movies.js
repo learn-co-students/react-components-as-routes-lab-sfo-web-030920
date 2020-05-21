97c5fba06b8800d173d84f2fbde3eaa953050f3d
caicindy87 <caicindy87@gmail.com>
2020-05-20 20:36:49 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+        <div>
+          <h3>Name: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <ul className="navbar">
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/movies">Movies</NavLink>
+      </li>
+      <li>
+        <NavLink to="/directors">Directors</NavLink>
+      </li>
+      <li>
+        <NavLink to="/actors">Actors</NavLink>
+      </li>
+    </ul>
   );
 };
 
